84001df4160db18bed8151fcae03174dd930aedd
Panos61 <panagiwtis.orovas@gmail.com>
2022-01-09 03:55:49 +0200

    Added audio player functionality

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,22 +1,22 @@
-import "./styles/login.style.scss";
-import { loginUrl } from "../utils/spotify_auth";
+import './styles/login.style.scss';
+import { loginUrl } from '../utils/spotify_auth';
 
 const Login: React.FC = () => {
-  return (
-    <div id="login-layout">
-      <img
-        key={Date.now()}
-        src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
-        alt="spotify-logo"
-      />
+	return (
+		<div id='login-layout'>
+			<img
+				key={Date.now()}
+				src='https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg'
+				alt='spotify-logo'
+			/>
 
-      <div>
-        <h3>Log in with your account.</h3>
+			<div>
+				<h3>Log in with your account.</h3>
 
-        <a href={loginUrl}>LOGIN WITH SPOTIFY</a>
-      </div>
-    </div>
-  );
+				<a href={loginUrl}>LOGIN WITH SPOTIFY</a>
+			</div>
+		</div>
+	);
 };
 
 export default Login;
